7b444e67d4f6a3f3af30ae8f19d703b16d16356b
Boateng Jephthah Agyenim <Boatengjephthah@gmail.com>
2022-08-30 18:22:03 +0000

    change 1

--- hangman.py
+++ hangman.py
@@ -1,4 +1,6 @@
 from tkinter import *
+import random as ran
+from tkinter import messagebox
 
 
 
@@ -21,8 +23,79 @@ color = "#F7F6DC"
 
 
 def check():
-    pass
+    with open("words.txt", "r") as game_words:
+        words = game_words.read().split(" , ")
+        secret_word = (ran.choice(words)).lower()
+        word_length = len(secret_word)
+        list_ = []
+        outword = ""
+        
+        for i in range(word_length):
+            list_.append("_")
+        
+        for w in list_:
+            outword += f"{w} "
+            
+        canvas.itemconfigure(word, text=outword)        
+            
+            
+        game_end = False
+        lives = 6
+        
+        while not game_end:
+    
+            user_guess = text.get()
 
+
+            if user_guess in list_:
+                messagebox.showwarning(f"Your guess ' {user_guess} ' is already in the list!")
+
+
+            for letter_position in range(word_length):
+                letter = secret_word[letter_position] 
+                if letter == user_guess:
+                    list_[letter_position] = letter 
+                    text.delete(0, END)
+                    
+            canvas.itemconfigure(word, text=list_)
+
+
+            if user_guess not in secret_word:
+                print(f"\nYour guess {user_guess} is not in the word, \nYou lose a life")
+                lives -= 1
+                if lives == 5:
+                    for columns in g.at5:
+                        print(columns, end="")
+                        t.sleep(0.002)
+                elif lives == 4:
+                    for columns in g.at4:
+                        print(columns, end="")
+                        t.sleep(0.002)
+                elif lives == 3:
+                    for columns in g.at3:
+                        print(columns, end="")
+                        t.sleep(0.002)
+                elif lives == 2:
+                    for columns in g.at2:
+                        print(columns, end="")
+                        t.sleep(0.002)
+                elif lives == 1:
+                    for columns in g.at1:
+                        print(columns, end="")
+                        t.sleep(0.002)
+                elif lives == 0:
+                    game_end = True
+                    for columns in g.at0:
+                        print(columns, end="")
+                        t.sleep(0.002)
+                    print(f"The word was {secret_word}!")
+                    print("\nSorry, You are out of lives. \nYou lose!\n")
+
+            if "_" not in list_:
+                game_end = True
+                print(f"\nThe word is {secret_word}!")
+                print("\nYou won!\n")
+            
 
 
 
@@ -67,7 +140,7 @@ canvas.create_image(
     image=img5
 )
 
-canvas.create_text(
+word = canvas.create_text(
     200,280,
     text="---------------",
     font=("arial", 28, "bold"),
